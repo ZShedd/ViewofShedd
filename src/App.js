2430779559942ffd92c7bff4f1aef23feba31369
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-17 13:34:30 -0700

    Reanmed files. Updated App.js with bg image and styling.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React from 'react';
+import PortfolioContainer from './components/PortfolioContainer';
+import Forest from './assets/images/Forest.jpg';
+import Footer from './components/Footer';
 
-const App = () => <PortfolioContainer />;
+const styles = {
+  body: {
+    backgroundColor: '#040404',
+  },
+  container: {
+    backgroundImage: `url(${Forest})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    // backgroundRepeat: 'repeat-y',
+    backgroundAttachment: 'fixed',
+    width: '80vw',
+    minHeight: '150vh',
+    marginLeft: '10%',
+  },
+};
+
+const App = () => {
+  return (
+    <div style={styles.body}>
+      <div style={styles.container}>
+        <PortfolioContainer />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
-
-
