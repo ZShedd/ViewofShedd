e0c9715eec059b55b050d82abcbb30bcaeb340c1
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-17 13:18:43 -0700

    Updated about page and added styling.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import Me from '../../assets/images/Moi.jpg';
+
+const styles = {
+  h1: {
+    margin: '20px',
+    flexDirection: 'row-wrap',
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  section: {
+    marginRight: '20px',
+    display: 'flex',
+    flexDirection: 'column-wrap',
+  },
+  img: {
+    width: '300px',
+  },
+};
 
 export default function About() {
   return (
-    <div>
-      <h1>Projects</h1>
-      <p>
-      Blank
-      </p>
+    <div style={styles.h1}>
+      <h1>About Me</h1>
+      <div style={styles.main}>
+        <section style={styles.section}>
+          <img
+            alt="me"
+            src={Me}
+            style={styles.img}
+          />
+        </section>
+        <section style={styles.section}>
+          <div>
+            <p>Full Stack Web Developer</p>
+            <p>Tech Enthusiast</p>
+            <p>Dog Lover</p>
+            <p>Adventurer</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
