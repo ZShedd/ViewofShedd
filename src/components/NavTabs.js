52713886d1c780d80c4332705b19290f6b10483c
Clown Supreme <zachary.h.shedd@gmail.com>
2023-04-17 12:49:26 -0700

    Restructured files. Created calls to render pages. created Navbar.

---
+++ src/components/NavTabs.js
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const styles = {
+  a: {
+    color: '#040404',
+  },
+  active: {
+    backgroundColor: '#676D87',
+  },
+};
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          style={styles.a}
+          onClick={() => handlePageChange('About')}
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+          style={styles.a}
+          onClick={() => handlePageChange('Portfolio')}
+          className={
+            currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          style={styles.a}
+          onClick={() => handlePageChange('Contact')}
+          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          style={styles.a}
+          onClick={() => handlePageChange('Resume')}
+          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import Resume from './pages/Resume';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('About');
+
+  const RenderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <NavTabs
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      <RenderPage />
+    </div>
+  );
+}
